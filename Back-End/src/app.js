a165d4b8d65902a4cf6143fb709ae3b7d2bced30
ZERO34802 <99256532+ZERO34802@users.noreply.github.com>
2025-09-22 10:26:22 +0530

    mounting CORS

--- Back-End/src/app.js
+++ Back-End/src/app.js
@@ -3,6 +3,12 @@ const express = require("express");
 const authRoutes = require("./routes/auth");
 const app = express();
 
+const cors = require("cors");
+const allow = [process.env.FRONTEND_ORIGIN];
+app.use(
+  cors({ origin: (origin, cb) => cb(null, !origin || allow.includes(origin)) })
+);
+
 app.get("/health", (req, res) => res.send("ok"));
 
 app.use(express.json());
