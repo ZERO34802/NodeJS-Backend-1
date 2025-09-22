7266d7906890c74b792ff3a3c7a4a407a13575c5
ZERO34802 <99256532+ZERO34802@users.noreply.github.com>
2025-09-22 09:47:25 +0530

    Delete src directory

--- src/middlewares/validate.js
+++
@@ -1,7 +0,0 @@
-const validate = (schema) => (req, res, next) => {
-  const r = schema.safeParse(req.body);
-  if (!r.success) return res.status(400).json({ errors: r.error.flatten() });
-  req.body = r.data;
-  next();
-};
-module.exports = { validate };
