3587cfd3cdeff8673d94eb2aaab670f26865e0c4
ZERO34802 <99256532+ZERO34802@users.noreply.github.com>
2025-09-22 10:18:24 +0530

    added a health check to the deployed backend-api

--- Back-End/src/app.js
+++ Back-End/src/app.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const authRoutes = require("./routes/auth");
 const app = express();
+
+app.get("/health", (req, res) => res.send("ok"));
+
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 module.exports = app;
